383adf172127bd394d2ae148a03e879909768621
Shujaat Azim <shujaat.azim@gmail.com>
2019-04-02 21:41:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-const facts = ["He was the last Beatle to learn to drive",
-"He was never a vegetarian",
-"He was a choir boy and boy scout",
-"He hated the sound of his own voice"
-];
 
 var newArr = [];
 
@@ -16,6 +11,11 @@ return newArr;
 }
 
 var newArr2 = [];
+const facts = ["He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
+];
 
 function johnLennonFacts(facts) {
   var counter = 0;

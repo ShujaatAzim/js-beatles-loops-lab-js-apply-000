283a296bdeedf45eec618da166e51f8850ae316d
Shujaat Azim <shujaat.azim@gmail.com>
2019-04-02 21:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
 var newArr = [];
 
 function theBeatlesPlay(musicians, instruments) {

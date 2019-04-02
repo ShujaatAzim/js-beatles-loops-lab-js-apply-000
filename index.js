921235205e81100c93e6d6472a30400292cbcb9e
Shujaat Azim <shujaat.azim@gmail.com>
2019-04-02 21:26:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var counter = 0;
 
 function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++) {
-    newArr.push(musicians[i] + instruments.indexOf(i))
+    newArr.push(musicians[i] + instruments[i]);
   }
 return newArr;
 }

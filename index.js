c5c21c7e3b447d0a3d904304692fcfd5ca7fefb0
Shujaat Azim <shujaat.azim@gmail.com>
2019-04-02 21:13:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+var musiciansArr = [John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instrumentsArr = [];
+var counter = 0;
+
+function theBeatlesPlay(musicians, instruments) {
+  
+}
